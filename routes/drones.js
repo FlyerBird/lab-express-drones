5527c8426e8e41b9b2627f0b5b1a7ce44a800130
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-06-10 23:49:55 +0200

    lab done

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,76 @@
 const express = require('express');
 const router = express.Router();
-
 // require the Drone model here
+const Drones = require ('../models/Drone.model')
 
-router.get('/drones', (req, res, next) => {
+router.get('/drones', async (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  try {
+    const drones = await Drones.find({})
+    res.render('drones/list', {drones})
+  } catch (error) {
+    next(error)
+  }
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  try {
+    res.render('drones/create-form')
+  } catch (error) {
+    next(error)
+  }
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post('/drones/create', async (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const { name, propellers, maxSpeed } = req.body;
+  const propellersNumber = parseInt(propellers);
+  const maxSpeedNumber = parseInt(maxSpeed);
+
+  try {
+    await Drones.create ({name, propellers:propellersNumber, maxSpeed:maxSpeedNumber})
+    res.redirect('/drones');
+  } catch (error) {
+    next(error)
+  }
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get('/drones/:id/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  try {
+    const dronesEdit = await Drones.findById(id)
+    res.render('drones/update-form', dronesEdit)
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:id/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  const { name, propellers, maxSpeed} = req.body;
+  const propellersNumber = parseInt(propellers);
+  const maxSpeedNumber = parseInt(maxSpeed);
+  try {
+    await Drones.findByIdAndUpdate(id, {name, propellers:propellersNumber, maxSpeed:maxSpeedNumber})
+    res.redirect('/drones');
+  } catch (error) {
+    next(error);
+  }
+  
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post('/drones/:id/delete', async (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const { id } = req.params;
+  try {
+    await Drones.findByIdAndDelete(id)
+    res.redirect('/drones');
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,61 @@
 // Iteration #1
+const mongoose = require ('mongoose')
+const Drones = require ('../models/Drone.model')
+
+const drones = [
+    {
+      name: "Creeper XL 500",
+      propellers: 3,
+      maxSpeed: 12,
+    },
+    {
+        name: "Racer 57",
+        propellers: 4,
+        maxSpeed: 20,
+      },
+      {
+        name: "Courier 3000i",
+        propellers: 6,
+        maxSpeed: 18,
+      }
+  
+  ];
+  
+  // Add here the script that will be run to actually seed the database (feel free to refer to the previous lesson)
+  
+  /*
+  mongoose.connect('mongodb://localhost/lab-express-cinema')
+  .then(x=> console.log(`connected to ${x.connection.name}`))
+  .then(()=> {
+      return Movies.create(movies)
+  })
+  .catch (e=> console.log(e))
+  .finally (() => {
+      mongoose.connection.close()
+  })
+  */
+
+
+  // ℹ️ package responsible to make the connection with mongodb
+// https://www.npmjs.com/package/mongoose
+
+
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    return Drones.create(drones)
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err); 
+  })
+  .finally (() => {
+    mongoose.connection.close()
+}
+  );
+
